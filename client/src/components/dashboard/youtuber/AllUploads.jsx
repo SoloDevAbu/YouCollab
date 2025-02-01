f703a2782a10149bf9225a3d5abbed2c9cd5ec34
SoloDevAbu <abubakkar2502@gmail.com>
2025-02-01 12:51:49 +0530

    Youtuber dashboard in progress

---
+++ client/src/components/dashboard/youtuber/AllUploads.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllUploads = () => {
+  return (
+    <div>AllUploads</div>
+  )
+}
+
+export default AllUploads
--- client/src/components/dashboard/youtuber/YoutuberDashboardRight.jsx
+++ client/src/components/dashboard/youtuber/YoutuberDashboardRight.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import RecentUpload from './RecentUpload'
+import AllUploads from './AllUploads'
+import PendingApproval from './PendingApproval'
+import Approved from './Approved'
+import Rejected from './Rejected'
 
-const YoutuberDashboardRight = () => {
+const YoutuberDashboardRight = ({selectedOption}) => {
   return (
     <div>
-      
+      {selectedOption === 'recent-uploads' && <RecentUpload/>}
+      {selectedOption === 'all-uploads' && <AllUploads/>}
+      {selectedOption === 'pending-approval' && <PendingApproval/>}
+      {selectedOption === 'approved' && <Approved/>}
+      {selectedOption === 'rejected' && <Rejected/>}
     </div>
   )
 }
